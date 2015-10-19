dac1192d0bdeae932e2b4652cc5bcf7ce89bee4e
syntaxfish <syntaxfish@gmail.com>
2015-10-19 16:45:49 +0900

    update route

--- web/router.ex
+++ web/router.ex
@@ -15,32 +15,34 @@ defmodule HaruElixirApi.Router do
 
   scope "/", HaruElixirApi do
     pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
   end
 
   scope "/v2", HaruElixirApi do
     pipe_through :api
 
     ## Entity
-    post "/classes/:classname", WriteController, :create_entity
-    put "/classes/:classname/:id", WriteController, :update_entity
-    delete "/classes/:classname/:id", WriteController, :delete_entity
-    delete "/classes/:classname", WriteController, :delete_class
+    post "/classes/:classname", EntityController, :create_entity
+    get "/classes/:classname", EntityController, :read_entities
+    get "/classes/:classname/:id", EntityController, :read_entity
+    put "/classes/:classname/:id", EntityController, :update_entity
+    delete "/classes/:classname/:id", EntityController, :delete_entity
+
+    ## Class
+    delete "/classes/:classname", ClassController, :delete_class
 
     ## Config
-    post "/config", WriteController, :create_config
-    put "/config", WriteController, :update_config
-    delete "/config", WriteController, :delete_config
+    post "/config", ConfigController, :create_config
+    get "/config", ConfigController, :read_config
+    put "/config", ConfigController, :update_config
+    delete "/config", ConfigController, :delete_config
 
     ## Review crawler
-    post "/reiview/fetch", WriteController, :fetch_review
-
+    post "/reiview/fetch", ReviewController, :fetch_review
 
     ## Push
-    post "/push", WriteController, :send_push
+    post "/push", PushController, :send_push
 
     ## Monetization
-    post "/monetization", WriteController, :create_monetiztion
+    post "/monetization", MonetizationController, :create_monetiztion
   end
 end
